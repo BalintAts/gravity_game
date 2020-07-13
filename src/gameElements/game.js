6851798863d8b44076c67dd624f6438c7efd2235
Bálint Áts <atsbalint@gmail.com>
2020-07-13 15:27:35 +0200

    create checLevelCompleted and levelcompleted signature

--- src/gameElements/game.js
+++ src/gameElements/game.js
@@ -5,6 +5,8 @@ class Game {
 
     level;
     currentLevelNumber = 0;
+    player;
+
 
     constructor(width, height) {
         this.width = width;
@@ -13,6 +15,17 @@ class Game {
 
     createLevel() {
         this.level = new Level(this.currentLevelNumber);
+    }
+
+    checkLevelCompleted() {
+        if (this.player.score === this.level.scoreToWin) {
+            this.levelCompleted();
+        }
+    }
+
+    levelCompleted() {
+        this.currentLevelNumber++;
+        this.createLevel(this.currentLevelNumber);
     }
 
     gameOver() {
