5c32d81f19a05560075afc6c12030023aca2746a
Bálint Áts <atsbalint@gmail.com>
2020-07-29 17:18:13 +0200

    Create state pattern for menu

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -5,30 +5,88 @@ import { IsLoggedInContext } from '../contexts/loginContext';
 const Menu = (props) => {
 
     const [isLoggedIn, setIsLoggedIn] = useContext(IsLoggedInContext);
+    // const [loginScreen, setLoginScreen] = useState(false);
+    const [menuState, setMenuState] = useState("notLoggedIn");
 
     const close = () => {
         props.onChange();
     }
 
     const loggingIn = () => {
-        setIsLoggedIn(true);
+        setMenuState("loggingIn");
     }
 
-    useEffect(() => {
+    const logInSend = () => {
+        setMenuState("loggedIn");
+    }
 
-    }, [isLoggedIn])
+    const loggingOut = () => {
+        setMenuState("notLoggedIn");
+    }
 
-
-
+    useEffect(() => { }, [isLoggedIn])
 
 
     return (
         <div className="modal_wrapper">
             <div className="modal_backdrop">
                 <div className="modal_box">
-                    {/* <MenucontentComponent/>   this is some dynamic stuff */}
-                    {/* placeholder content: */}
-                    {isLoggedIn ? (
+                    {/* {isLoggedIn ? (
+                        <>
+                            <h1 style={{ color: "white" }}>Logged in</h1>
+                            {strysomething}
+                            <ul>
+                                <li>
+                                    <button onClick={close}>New Game</button>
+                                </li>
+                                <li>
+                                    <button onClick={close}>Load game</button>
+                                </li>
+                                <li>
+                                    <button onClick={loggingOut}>Log out</button>
+                                </li>
+                            </ul>
+                        </>
+                    ) : (
+                            <>
+                                <h1 style={{ color: "white" }}>Not logged in</h1>
+                                <ul>
+                                    <li>
+                                        <button onClick={loggingIn}>Play without saving progress</button>
+                                    </li>
+                                    <li>
+                                        <button onClick={loggingIn}>Login</button>
+                                    </li>
+                                    <li>
+                                        <button onClick={loggingIn}>Register to save progress</button>
+                                    </li>
+                                </ul>
+                            </>
+                        )} */}
+                    {menuState === "notLoggedIn" &&
+                        <>
+                            <h1 style={{ color: "white" }}>Not logged in</h1>
+                            <ul>
+                                <li>
+                                    <button onClick={close}>Play without saving progress</button>
+                                </li>
+                                <li>
+                                    <button onClick={loggingIn}>Login</button>
+                                </li>
+                                <li>
+                                    <button onClick={loggingIn}>Register to save progress</button>
+                                </li>
+                            </ul>
+                        </>
+                    }
+                    {menuState === "loggingIn" &&
+                        <>
+                            <h1 style={{ color: "white" }}>Login Page</h1>
+                            <button onClick={logInSend}>Login</button>
+                        </>
+
+                    }
+                    {menuState === "loggedIn" &&
                         <>
                             <h1 style={{ color: "white" }}>Logged in</h1>
                             <ul>
@@ -39,20 +97,11 @@ const Menu = (props) => {
                                     <button onClick={close}>Load game</button>
                                 </li>
                                 <li>
-                                    <button onClick={close}>Log out</button>
+                                    <button onClick={loggingOut}>Log out</button>
                                 </li>
                             </ul>
                         </>
-                    ) : (
-                            <>
-                                <h1 style={{ color: "white" }}>Not logged in</h1>
-                                <ul>
-                                    <li>
-                                        <button onClick={loggingIn}>Login</button>
-                                    </li>
-                                </ul>
-                            </>
-                        )}
+                    }
 
                 </div>
             </div>
