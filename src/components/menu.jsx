c5fe1a0a70a082789ca612cfe4556b86acd2eb4d
Bálint Áts <atsbalint@gmail.com>
2020-08-14 14:20:59 +0200

    Create registerForm

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, forwardRef, useContext } from 'react';
 import '../styles/menuStyle.css';
 import { IsLoggedInContext } from '../contexts/loginContext';
 import { useForm } from "react-hook-form";
+import axios from 'axios';
 
 const Menu = (props) => {
 
@@ -16,6 +17,7 @@ const Menu = (props) => {
 
     const loggingIn = () => {
         setMenuState("loggingIn");
+        ;
     }
 
     const logInSucces = () => {
@@ -28,7 +30,21 @@ const Menu = (props) => {
 
     const onLogin = data => {
         console.log(data);
+        axios.post(`localhost:8080/${data.userName}`)
+            .then(response => { console.log(response) })
+            .catch(error => { console.log(error) });
+        logInSucces();
     }
+
+    const registerFrom = () => {
+        setMenuState("registerFrom");
+    }
+
+    const onRegister = data => {
+
+    }
+
+
 
     useEffect(() => { }, [isLoggedIn])
 
@@ -48,7 +64,7 @@ const Menu = (props) => {
                                     <button onClick={loggingIn}>Login</button>
                                 </li>
                                 <li>
-                                    <button onClick={loggingIn}>Register to save progress</button>
+                                    <button onClick={registerFrom}>Register to save progress</button>
                                 </li>
                             </ul>
                         </>
@@ -57,7 +73,21 @@ const Menu = (props) => {
                         <>
                             <form onSubmit={handleSubmit(onLogin)}>
                                 <h1 style={{ color: "white" }}>Login Page</h1>
-                                <input type="text" placeholder="Username" name="userName" ref={register} />
+                                <input type="text" placeholder="Username" name="userName" ref={register({ required: "NAME REQUIRED, YOU MORON" })} />
+                                {errors.userName && <p style={{ color: "orange" }}>{errors.userName.message}</p>}
+                                <input type="submit" />
+                            </form>
+                        </>
+
+                    }
+                    {menuState === "registerFrom" &&
+                        <>
+                            <form onSubmit={handleSubmit(onRegister)}>
+                                <h1 style={{ color: "white" }}>Register page</h1>
+                                <input type="text" placeholder="Username" name="userName" ref={register({ required: "NAME REQUIRED, YOU MORON" })} />
+                                {errors.userName && <p style={{ color: "orange" }}>{errors.userName.message}</p>}
+                                <input type="text" placeholder="password" name="password" ref={register({ required: "PASSWORD REQUIRED, YOU MORON" })} />
+                                {errors.password && <p style={{ color: "orange" }}>{errors.password.message}</p>}
                                 <input type="submit" />
                             </form>
                         </>
