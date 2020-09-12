1983c87d677a6c196bb00a7001f52ddbcf848830
Bálint Áts <atsbalint@gmail.com>
2020-09-12 20:05:02 +0200

    Change size of collectables

--- src/data/levelsData.js
+++ src/data/levelsData.js
@@ -5,9 +5,9 @@ let levelsData =
             { thing: "uranus", x: 1100, y: 450, radius: 100, mass: 80, gravitable: true, collectable: false, color: "green", visible: true },
             { thing: "jupiter", x: 600, y: 700, radius: 70, mass: 70, gravitable: true, collectable: false, color: "green", visible: true },
             { thing: "mars", x: 600, y: 100, radius: 70, mass: 70, gravitable: true, collectable: false, color: "green", visible: true },
-            { thing: "", x: 600, y: 450, radius: 50, mass: 50, gravitable: false, collectable: true, color: "yellow", visible: true },
-            { thing: "", x: 700, y: 700, radius: 50, mass: 50, gravitable: false, collectable: true, color: "yellow", visible: true },
-            { thing: "", x: 1200, y: 400, radius: 50, mass: 50, gravitable: false, collectable: true, color: "yellow", visible: true },
+            { thing: "", x: 700, y: 450, radius: 30, mass: 50, gravitable: false, collectable: true, color: "yellow", visible: true },
+            { thing: "", x: 800, y: 700, radius: 30, mass: 50, gravitable: false, collectable: true, color: "yellow", visible: true },
+            { thing: "", x: 1300, y: 400, radius: 30, mass: 50, gravitable: false, collectable: true, color: "yellow", visible: true },
 
         ],
         [
@@ -15,9 +15,9 @@ let levelsData =
             { x: 200, y: 450, radius: 100, mass: 80, gravitable: true, collectable: false, color: "green", visible: true },
             { x: 1200, y: 200, radius: 50, mass: 50, gravitable: true, collectable: false, color: "green", visible: true },
             { x: 600, y: 1000, radius: 50, mass: 50, gravitable: true, collectable: false, color: "green", visible: true },
-            { x: 600, y: 450, radius: 50, mass: 50, gravitable: false, collectable: true, color: "yellow", visible: true },
-            { x: 1200, y: 100, radius: 50, mass: 50, gravitable: false, collectable: true, color: "yellow", visible: true },
-            { x: 100, y: 500, radius: 50, mass: 50, gravitable: false, collectable: true, color: "yellow", visible: true },
+            { x: 700, y: 450, radius: 20, mass: 50, gravitable: false, collectable: true, color: "yellow", visible: true },
+            { x: 1300, y: 100, radius: 20, mass: 50, gravitable: false, collectable: true, color: "yellow", visible: true },
+            { x: 200, y: 500, radius: 20, mass: 50, gravitable: false, collectable: true, color: "yellow", visible: true },
         ],
         [{}, {}, {}, {}
         ],
